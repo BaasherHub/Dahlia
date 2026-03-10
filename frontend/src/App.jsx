a77d6bb56b429be32e8a94014d470270457f21d6
BaasherHub <baasher@hotmail.com>
2026-03-10 06:20:44 +0400

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,21 @@
-import { Routes, Route } from 'react-router-dom';
-import { CartProvider } from './context/CartContext.jsx';
-import { ThemeProvider } from './context/ThemeContext.jsx';
-import Nav from './components/Nav.jsx';
-import Footer from './components/Footer.jsx';
-import HomePage from './pages/HomePage.jsx';
-import GalleryPage from './pages/GalleryPage.jsx';
-import PaintingPage from './pages/PaintingPage.jsx';
-import CollectionPage from './pages/CollectionPage.jsx';
-import CartPage from './pages/CartPage.jsx';
-import CheckoutPage from './pages/CheckoutPage.jsx';
-import OrderSuccessPage from './pages/OrderSuccessPage.jsx';
-import AboutPage from './pages/AboutPage.jsx';
-import CommissionsPage from './pages/CommissionsPage.jsx';
-import AdminPage from './pages/AdminPage.jsx';
+import ErrorBoundary from './components/ErrorBoundary';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import Footer from './components/Footer';
+// ... other imports
 
-export default function App() {
+function App() {
   return (
-    <ThemeProvider>
-      <CartProvider>
-        <Nav />
+    <ErrorBoundary>
+      <Router>
+        <Navigation />
         <Routes>
-          <Route path="/"                   element={<HomePage />} />
-          <Route path="/gallery"            element={<GalleryPage />} />
-          <Route path="/paintings/:id"      element={<PaintingPage />} />
-          <Route path="/collections/:id"    element={<CollectionPage />} />
-          <Route path="/cart"               element={<CartPage />} />
-          <Route path="/checkout"           element={<CheckoutPage />} />
-          <Route path="/order/success"      element={<OrderSuccessPage />} />
-          <Route path="/about"              element={<AboutPage />} />
-          <Route path="/commissions"        element={<CommissionsPage />} />
-          <Route path="/admin"              element={<AdminPage />} />
+          {/* Your routes here */}
         </Routes>
         <Footer />
-      </CartProvider>
-    </ThemeProvider>
+      </Router>
+    </ErrorBoundary>
   );
 }
+
+export default App;
