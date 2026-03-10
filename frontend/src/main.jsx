1485826f0ca4e6576120cf1a5a5d22d32f663658
BaasherHub <baasher@hotmail.com>
2026-03-10 14:27:42 +0400

    Wrap App component with ThemeProvider and CartProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
+import { ThemeProvider } from './context/ThemeProvider.jsx';
+import { CartProvider } from './context/CartContext.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
